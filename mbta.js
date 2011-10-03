6b59a2458d98af0af9539175e1446723a5d25243
Daniel R. Craig <drcraig@193143-qb0-1.dfw1.staging.slicehost.com>
2011-10-03 18:53:43 +0000

    Sort by time at starting station, and remove debug prints

--- mbta.js
+++ mbta.js
@@ -3,20 +3,31 @@ var populate_table = function ($table, data) {
     $.map($table.find('th.PlatformKey'), function(e) { platformKeys.push(e.id);});
 
     // Get the trip ids for the starting station
+    var tripIdsFromStart = []
     var tripsFromStart = []
     data.map( function(element) {
         if( element.PlatformKey === platformKeys[0] ) {
-            tripsFromStart.push(element.Trip)
+            tripIdsFromStart.push(element.Trip);
+            tripsFromStart.push(element);
         }
     });
+
+    // Sort those by the time they arrive at the starting station
+    tripsFromStart.sort( function (a,b) {
+        var da = Date.parse(a.Time);
+        var db = Date.parse(b.Time);
+        if ( da > db ) return 1;
+        if ( da < db ) return -1;
+        return 0;
+    });
     
-    if ( tripsFromStart.length === 0) {
+    if ( tripIdsFromStart.length === 0) {
         $table.after("<em>No times reported</em>");
     } else {
         // Get the info for each desired station for each trip that comes through the starting station
         var trips = {}
         data.map( function(element) {
-           if( platformKeys.indexOf(element.PlatformKey) >= 0 && tripsFromStart.indexOf(element.Trip) >= 0){
+           if( platformKeys.indexOf(element.PlatformKey) >= 0 && tripIdsFromStart.indexOf(element.Trip) >= 0){
                if( trips[element.Trip] === undefined) {
                    trips[element.Trip] = {}
                }
@@ -25,17 +36,13 @@ var populate_table = function ($table, data) {
         });
 
         // Create a row in the table for each train
-        for( var i in trips) {
-            var trip = trips[i]
+        for( var i in tripsFromStart) {
+            var trip = trips[tripsFromStart[i].Trip]
             var $row = $("<tr></tr>");
-            console.log(i);
             for( j in platformKeys ) {
                 var platformKey = platformKeys[j];
                 var station = trip[platformKey];
-                console.log(station);
-                console.log(station.Time);
                 var time = Date.parse(station.Time);
-                console.log(time);
                 var $cell = $("<td>"+time.toString("h:mm tt")+"</td>");
                 $cell.append('<br />')
                 $cell.append('<abbr class="timeago" title="'+time.toISOString()+'">'+station.TimeRemaining+'<abbr>');
